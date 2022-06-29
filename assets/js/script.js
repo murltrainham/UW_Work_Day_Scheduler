23019cac1e3b3f5ddcff9fe6a66fa72d524ece3f
Murl Trainham <murltrainham@gmail.com>
2022-06-29 13:56:41 -0700

    working

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,49 @@ WHEN I refresh the page
 **Misc / Notes from zoom recordings: 
 9-5 timeline
 Fully functional CSS just need to add correct HTML 
+add id timeEntries to class "container" in HTML 
 
+for (let i = 9; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
+
+HTML data attributes around the clicked button
 
 **Workflow:
 HTML: add timeblocks @ ln 23 (9-5)
+id current day element
 CSS:
 JavaScript:
 (1) 
 
 */
 console.log("test");
+
+for (let i = 9; i < 5; i++) {
+
+    var key="hour-"+i;
+    var data ="asdf";
+
+//template literal show with append HTML to the page
+var template = `
+<div class="row">
+  <div>
+    ${i}AM
+  </div>
+  <div>
+    <textarea></textarea>
+  </div>
+  <div>
+    <button dataHour="${i}">Save</button>
+  </div>
+</div>
+
+`;
+}
+
+//event listner on save click
+containerEl.on("click", function(event) {
+
+    event.target.matches("button");
+
+});
