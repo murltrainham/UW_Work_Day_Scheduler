f0c6e530ffc940d5c585660cd782914c895918b3
Murl Trainham <murltrainham@gmail.com>
2022-06-29 13:02:46 -0700

    link css/js update Ac/Misc/Wf

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/* 
+**Acceptance criteria:
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+(1)THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+(2)THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+(3)THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+(4)THEN I can enter an event
+WHEN I click the save button for that time block
+(5)THEN the text for that event is saved in local storage
+WHEN I refresh the page
+(6)THEN the saved events persist
+
+**Misc / Notes from zoom recordings: 
+9-5 timeline
+Fully functional CSS just need to add correct HTML 
+
+
+**Workflow:
+HTML: add timeblocks @ ln 23 (9-5)
+CSS:
+JavaScript:
+(1) 
+
+*/
+console.log("test");
