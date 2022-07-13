94a012cf6c83164c50871d97667d4790923cdc77
Murl Trainham <murltrainham@gmail.com>
2022-07-13 13:21:26 -0700

    Update JS
    Finally got the time blocks to change color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* 
+/*
 **Acceptance criteria:
 
 GIVEN I am using a daily planner to create a schedule
@@ -15,37 +15,6 @@ WHEN I click the save button for that time block
 WHEN I refresh the page
 (6)THEN the saved events persist
 
-**Misc / Notes from zoom recordings: 
-9-5 timeline
-Fully functional CSS just need to add correct HTML 
-add id timeEntries to class "container" in HTML 
-
-for (let i = 9; i < 5; i++) {
-  text += "The number is " + i + "<br>";
-}
-
-HTML data attributes around the clicked button
-
-**Workflow:
-HTML: add timeblocks @ ln 23 (9-5)
-id current day element
-CSS:
-JavaScript:
-(1) 
-
-// localStorage.setItem(key, value);
-
-add current moment day/time 
-global variables
-add click listner
-for loop cycle userinput block color change
-
-# = jquery id attribute element selector
-
-In jQuery, the class and ID selectors are the same as in CSS. If you want to select elements with a certain class, use a dot (.) and the class name.
-If you want to select elements with a certain ID, use the hash symbol (#) and the ID name. 
-Note that HTML is not case-sensitive, therefore it is best practice to keep HTML markup and CSS selectors lowercase.
-
 https://www.freecodecamp.org/news/jquery-selectors/
 
 https://www.w3schools.com/js/js_json.asp
@@ -60,27 +29,101 @@ https://javascript.info/bubbling-and-capturing
 
 https://api.jquery.com/category/manipulation/
 
-overall goal function
+https://javascript.info/event-delegation
+
+https://www.tutorialspoint.com/jquery/jquery-css.htm
+
+https://www.sitepoint.com/jquery-each-function-examples/
+
+https://www.freecodecamp.org/news/javascript-foreach-how-to-loop-through-an-array-in-js/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
 
+
+//$(".timeblock").addClass("future");
+//$(".hour").addClass("future");
+//$(".description").addClass("future");
 */
-// empty global var / current time retreived from moment
-let currentTime = moment().hour();
-console.log(currentTime);
-// Display current date/time in header (p id = "currentDay" class = "lead")
-// variable defining moment day/time and format
-var presentTime = moment().format("LLLL");
-// console.log(presentTime) to ensure moment is working
-console.log(presentTime);
-// select currentDay HTML element and render our presentTime variable 
-$("#currentDay").html(presentTime);
 
-//change text area bg color loop based on presentTime check against static HTML times
-var colorLoop = function() {
+var date = new Date();
+var dateHours = date.getHours();
+$("#currentDay").html(date);
+console.log(dateHours);
 
-  $("#timeEntries")
+function conditionalTsunami() {
 
+  if (dateHours === 9) {
+    $("#9").addClass("present");
+  } else if (dateHours > 9) {
+    $("#9").addClass("past");
+  } else {
+    $("#9").addClass("future");
+  }
 
-};
+  if (dateHours === 10) {
+    $("#10").addClass("present");
+  } else if (dateHours > 10) {
+    $("#10").addClass("past");
+  } else {
+    $("#10").addClass("future");
+  }
 
-colorLoop();
+  if (dateHours === 11) {
+    $("#11").addClass("present");
+  } else if (dateHours > 11) {
+    $("#11").addClass("past");
+  } else {
+    $("#11").addClass("future");
+  }
+
+  if (dateHours === 12) {
+    $("#12").addClass("present");
+  } else if (dateHours > 12) {
+    $("#12").addClass("past");
+  } else {
+    $("#12").addClass("future");
+  }
+
+  if (dateHours === 13) {
+    $("#13").addClass("present");
+  } else if (dateHours > 13) {
+    $("#13").addClass("past");
+  } else {
+    $("#13").addClass("future");
+  }
+
+  if (dateHours === 14) {
+    $("#14").addClass("present");
+  } else if (dateHours > 14) {
+    $("#14").addClass("past");
+  } else {
+    $("#14").addClass("future");
+  }
+
+  if (dateHours === 15) {
+    $("#15").addClass("present");
+  } else if (dateHours > 15) {
+    $("#15").addClass("past");
+  } else {
+    $("#15").addClass("future");
+  }
+
+  if (dateHours === 16) {
+    $("#16").addClass("present");
+  } else if (dateHours > 16) {
+    $("#16").addClass("past");
+  } else {
+    $("#16").addClass("future");
+  }
+
+  if (dateHours === 17) {
+    $("#17").addClass("present");
+  } else if (dateHours > 17) {
+    $("#17").addClass("past");
+  } else {
+    $("#17").addClass("future");
+  }
+}
+conditionalTsunami();
+
