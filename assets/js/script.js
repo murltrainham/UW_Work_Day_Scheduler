392fac9e0d34848059183d8c0a7f11eac04b1a3f
Murl Trainham <murltrainham@gmail.com>
2022-07-13 16:52:55 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,24 +136,103 @@ conditionalTsunami();
 }
 loadOrama();
 */
+function loadOrama (){
+
+unStringify9 = JSON.parse(window.localStorage.getItem("input9"));
+output9 = document.getElementById("9");
+output9.value = unStringify9;
+
+unStringify10 = JSON.parse(window.localStorage.getItem("input10"));
+output10 = document.getElementById("10");
+output10.value = unStringify10;
+
+unStringify11 = JSON.parse(window.localStorage.getItem("input11"));
+output11 = document.getElementById("11");
+output11.value = unStringify11;
+
+unStringify12 = JSON.parse(window.localStorage.getItem("input12"));
+output12 = document.getElementById("12");
+output12.value = unStringify12;
+
+unStringify13 = JSON.parse(window.localStorage.getItem("input13"));
+output13 = document.getElementById("13");
+output13.value = unStringify13;
+
+unStringify14 = JSON.parse(window.localStorage.getItem("input14"));
+output14 = document.getElementById("14");
+output14.value = unStringify14;
+
+unStringify15 = JSON.parse(window.localStorage.getItem("input15"));
+output15 = document.getElementById("15");
+output15.value = unStringify15;
+
+unStringify16 = JSON.parse(window.localStorage.getItem("input16"));
+output16 = document.getElementById("16");
+output16.value = unStringify16;
+
+unStringify17 = JSON.parse(window.localStorage.getItem("input17"));
+output17 = document.getElementById("17");
+output17.value = unStringify17;
+
+}
+loadOrama();
+
 
 function saveOrama (){
 
-input9 = ("");
-stringify9 = [];
 btn9 = document.getElementById("saveBtn9");
-
-
 btn9.addEventListener("click", function(){
-  input9 = document.getElementById("9").value
+  input9 = document.getElementById("9").value;
   localStorage.setItem("input9", JSON.stringify(input9));
 });
 
+btn10 = document.getElementById("saveBtn10");
+btn10.addEventListener("click", function(){
+  input10 = document.getElementById("10").value;
+  localStorage.setItem("input10", JSON.stringify(input10));
+});
 
-//retreive localdata
-//listen for savebtn click
-//save typed data in local storage
+btn11 = document.getElementById("saveBtn11");
+btn11.addEventListener("click", function(){
+  input11 = document.getElementById("11").value;
+  localStorage.setItem("input11", JSON.stringify(input11));
+});
 
+btn12 = document.getElementById("saveBtn12");
+btn12.addEventListener("click", function(){
+  input12 = document.getElementById("12").value;
+  localStorage.setItem("input12", JSON.stringify(input12));
+});
+
+btn13 = document.getElementById("saveBtn13");
+btn13.addEventListener("click", function(){
+  input13 = document.getElementById("13").value;
+  localStorage.setItem("input13", JSON.stringify(input13));
+});
+
+btn14 = document.getElementById("saveBtn14");
+btn14.addEventListener("click", function(){
+  input14 = document.getElementById("14").value;
+  localStorage.setItem("input14", JSON.stringify(input14));
+});
+
+btn15 = document.getElementById("saveBtn15");
+btn15.addEventListener("click", function(){
+  input15 = document.getElementById("15").value;
+  localStorage.setItem("input15", JSON.stringify(input15));
+});
+
+btn16 = document.getElementById("saveBtn16");
+btn16.addEventListener("click", function(){
+  input16 = document.getElementById("16").value;
+  localStorage.setItem("input16", JSON.stringify(input16));
+});
+
+btn17 = document.getElementById("saveBtn17");
+btn17.addEventListener("click", function(){
+  input17 = document.getElementById("17").value;
+  localStorage.setItem("input17", JSON.stringify(input17));
+});
 
 
 }
